58dd0b3c9cc4a11d0dc7106d2760d3b51943e141
Leahjen <97486491+Leahjen@users.noreply.github.com>
2022-02-14 23:23:06 +0100

    fourth version
    - fixed an issue whereby virt-manager would not install

--- desktop.sh
+++ desktop.sh
@@ -43,12 +43,10 @@ usermod -a -G realtime $useracc2
 echo "*      soft      memlock      unlimited" >> /etc/security/limits.conf
 echo "*      soft      memlock      unlimited" >> /etc/security/limits.conf
 #VMs(1)
-pacman -S dnsmasq ebtables qemu libvirt virt-manager --noconfirm
+pacman -S dnsmasq qemu libvirt virt-manager -noconfirm
+pacman -S ebtables
 usermod -G libvirt -a $useracc2
 systemctl enable libvirtd.service
-systemctl restart libvirtd.service
-systemctl restart firewalld.service
-firewall-cmd --add-service libvirt --zone=libvirt
 #VMs(2)
 pacman -S virtualbox-host-dkms virtualbox virtualbox-guest-iso --noconfirm
 usermod -G vboxusers -a $useracc2
